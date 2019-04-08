83a52dd93ba9fa359f1042dac94db98ee68e96bd
zhangwenxu <1079167478@qq.com>
2019-04-08 16:11:48 +0800

    根据阿里规约，修改注释

--- src/main/java/com/haha/write/service/IRemoteWriteService.java
+++ src/main/java/com/haha/write/service/IRemoteWriteService.java
@@ -9,11 +9,32 @@ import prometheus.Remote;
  */
 public interface IRemoteWriteService {
 
-    void resolveRequest(byte[] data,String version);
+    /**
+     * @param data    : 数据
+     * @param version :Prometheus版本
+     * @description: 处理Prometheus发来的请求
+     * @create by: haha time: 2019/4/8 16:05
+     */
+    void resolveRequest(byte[] data, String version);
 
+    /**
+     * @param request:Prometheus数据
+     * @description: 把请求内容json存储到kafka
+     * @create by: haha  time: 2019/4/8 16:06
+     */
     void load2Kafka(Remote.WriteRequest request);
 
+    /**
+     * @param request:Prometheus数据
+     * @description: 把请求内容json存储到oracle
+     * @create by: haha  time: 2019/4/8 16:06
+     */
     void load2ORAC(Remote.WriteRequest request);
 
+    /**
+     * @param request:Prometheus数据
+     * @description: 把请求内容json存储到mysql
+     * @create by: haha  time: 2019/4/8 16:06
+     */
     void load2MYSQL(Remote.WriteRequest request);
 }
